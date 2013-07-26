249c8120096e22f57b6d4715e85437c2a4f9e39c
root <root@aegir.voicecom.bg>
2013-07-27 01:55:48 +0300

    psycopg2-2.5.1-1.git

---
+++ learn.db.py
@@ -0,0 +1,14 @@
+# 2013-07-27.00.46.56
+############################################################################################
+# cd /opt/proceed.bsms/psycopg2-2.5.1-1
+# git remote add origin ssh://git@bitbucket.org/seikath/aegir.bsms.psycopg2-2.5.1-1.git
+# git push -u origin --all   # to push up the repo for the first time
+############################################################################################
+# OR start from scratch
+# mkdir /path/to/your/project
+# cd /path/to/your/project
+# git init
+# git remote add origin ssh://git@bitbucket.org/seikath/aegir.bsms.psycopg2-2.5.1-1.git
+############################################################################################
+
+import psycopg2
