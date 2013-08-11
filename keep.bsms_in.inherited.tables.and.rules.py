e79a196c707f5f3f7530d18881b8b5b4ba6b7e8f
seikath <seikath@gmail.com>
2013-08-11 20:51:02 +0300

    added variables in separated file

--- keep.bsms_in.inherited.tables.and.rules.py
+++ keep.bsms_in.inherited.tables.and.rules.py
@@ -17,7 +17,9 @@ config = ConfigParser.SafeConfigParser()
 # get the config at the db.config file in the same directory
 config_file=os.path.dirname(os.path.abspath(__file__))+'/'+os.path.basename(os.path.dirname(os.path.abspath(__file__)))+'.cfg'
 
-import keep.bsms_in.inherited.tables.and.rules.inc
+#from keep.bsms_in.inherited.tables.and.rules.inc import *
+
+from vars import *
 
 try:
 	config.read(config_file)
@@ -44,28 +46,28 @@ except:
 # sys.exit (0)
 cur_aegir = conn_aegir.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-query_rule = ("""
-SELECT n.nspname AS schemaname, c.relname AS tablename, r.rulename, r.ev_enabled as active,
-    pg_get_ruledef(r.oid) AS definition
-   FROM pg_rewrite r
-   JOIN pg_class c ON c.oid = r.ev_class
-   LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
-  WHERE true 
--- and r.rulename <> '_RETURN'::name
-and c.relname = %(tablename)s
-and  r.rulename like 'route_rule_bsms_in_p' || extract(year from now()+ interval '%(week)s week') || '%%'
--- and r.ev_enabled <> %(enabled)s 
-and r.rulename  in (
-	select  
-	'route_rule_bsms_in_p' 
-	|| extract(year from now()+ interval '%(week)s week') 
-	|| 'w' || extract(week from now() + interval '%(week)s week') 
-	as  rulename
-	)
-order by r.rulename 
-limit 1
-;
-""")
+#query_rule = ("""
+#SELECT n.nspname AS schemaname, c.relname AS tablename, r.rulename, r.ev_enabled as active,
+    #pg_get_ruledef(r.oid) AS definition
+   #FROM pg_rewrite r
+   #JOIN pg_class c ON c.oid = r.ev_class
+   #LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+  #WHERE true 
+#-- and r.rulename <> '_RETURN'::name
+#and c.relname = %(tablename)s
+#and  r.rulename like 'route_rule_bsms_in_p' || extract(year from now()+ interval '%(week)s week') || '%%'
+#-- and r.ev_enabled <> %(enabled)s 
+#and r.rulename  in (
+	#select  
+	#'route_rule_bsms_in_p' 
+	#|| extract(year from now()+ interval '%(week)s week') 
+	#|| 'w' || extract(week from now() + interval '%(week)s week') 
+	#as  rulename
+	#)
+#order by r.rulename 
+#limit 1
+#;
+#""")
 
 
 
@@ -116,44 +118,44 @@ else:
 	if vc_debug : print "["+str(datetime.datetime.now())+"] : We dont have rule serving {0!s} weeks".format(weeks_to_deactivate)
 	
 
-query_table_is_inherited = ("""
+#query_table_is_inherited = ("""
 
-SELECT 
-pg_inherits.*
-, c.relname AS child
-, p.relname AS parent
-FROM
-    pg_inherits JOIN pg_class AS c ON (inhrelid=c.oid)
-    JOIN pg_class as p ON (inhparent=p.oid) 
-    where true 
-    and p.relname = %(table_name_base)s
-    and c.relname = %(inherited_table)s
-;
-""")
-query_table_exists = ("""
-select 
-	table_name 
-	from information_schema.tables 
-	where true 
-	and table_name=%(inherited_table)s
-;
-""")
+#SELECT 
+#pg_inherits.*
+#, c.relname AS child
+#, p.relname AS parent
+#FROM
+    #pg_inherits JOIN pg_class AS c ON (inhrelid=c.oid)
+    #JOIN pg_class as p ON (inhparent=p.oid) 
+    #where true 
+    #and p.relname = %(table_name_base)s
+    #and c.relname = %(inherited_table)s
+#;
+#""")
+#query_table_exists = ("""
+#select 
+	#table_name 
+	#from information_schema.tables 
+	#where true 
+	#and table_name=%(inherited_table)s
+#;
+#""")
 
-alter_table_inherit = ("""
-alter table 
-	%s 
-	%s inherit
-	%s
-;
-""")
+#alter_table_inherit = ("""
+#alter table 
+	#%s 
+	#%s inherit
+	#%s
+#;
+#""")
 
-create_trigger_on_table_inherited = ("""
-CREATE TRIGGER counters_%s
-  AFTER INSERT OR DELETE
-  ON %s
-  FOR EACH ROW
-  EXECUTE PROCEDURE update_counters_bsmsin_trigger();
-""")
+#create_trigger_on_table_inherited = ("""
+#CREATE TRIGGER counters_%s
+  #AFTER INSERT OR DELETE
+  #ON %s
+  #FOR EACH ROW
+  #EXECUTE PROCEDURE update_counters_bsmsin_trigger();
+#""")
 
 for week in xrange(1,5):
 	inherited_table = (datetime.datetime.today() + datetime.timedelta(weeks=week)).strftime("bsms_in_p%Yw%U")
