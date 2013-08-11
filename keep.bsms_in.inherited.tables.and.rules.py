52f8b35250f832c7e6867c6292324a5580f3978a
seikath <seikath@gmail.com>
2013-08-11 23:00:31 +0300

    added deactivated rule hek for the tables 5 weeks ahead

--- keep.bsms_in.inherited.tables.and.rules.py
+++ keep.bsms_in.inherited.tables.and.rules.py
@@ -13,13 +13,11 @@ os.environ['PGCONNECT_TIMEOUT'] = '5'
 
 #################################################
 ### CONFIG START 
+# import local vars - SQL templates
+from vars import *
 config = ConfigParser.SafeConfigParser()
 # get the config at the db.config file in the same directory
 config_file=os.path.dirname(os.path.abspath(__file__))+'/'+os.path.basename(os.path.dirname(os.path.abspath(__file__)))+'.cfg'
-
-#from keep.bsms_in.inherited.tables.and.rules.inc import *
-
-from vars import *
 
 try:
 	config.read(config_file)
@@ -29,13 +27,18 @@ try:
 	table_name_base = config.get('rule-conf','table_name_base')
 	vc_debug = config.getboolean('rule-conf','vc_debug')
 	vc_sql_debug = config.getboolean('rule-conf','vc_sql_debug')
+	vc_sql_debug = False
 	if vc_sql_debug : print "["+str(datetime.datetime.now())+"] : db config : {0!s}".format(config_aegir)
 except ConfigParser.Error, e:
 	print "["+str(datetime.now())+"] : " + "Error : %s" % (e)
 	sys.exit(1)
 	
-vc_sql_debug = False
+if not vc_debug : print "["+str(datetime.datetime.now())+"] : Debug set to false at " + os.path.abspath(__file__) 
+### CONFIG ENDS
+#################################################
 
+
+### Initiate db link 
 try:
     conn_aegir = psycopg2.connect(**config_aegir)
     conn_aegir.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
@@ -43,121 +46,55 @@ except:
     print "["+str(datetime.now())+"] : Not able to connect:: %s !!".format(config_aegir)
     sys.exit(1)
 
-# sys.exit (0)
+### Open db cursor
 cur_aegir = conn_aegir.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-#query_rule = ("""
-#SELECT n.nspname AS schemaname, c.relname AS tablename, r.rulename, r.ev_enabled as active,
-    #pg_get_ruledef(r.oid) AS definition
-   #FROM pg_rewrite r
-   #JOIN pg_class c ON c.oid = r.ev_class
-   #LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
-  #WHERE true 
-#-- and r.rulename <> '_RETURN'::name
-#and c.relname = %(tablename)s
-#and  r.rulename like 'route_rule_bsms_in_p' || extract(year from now()+ interval '%(week)s week') || '%%'
-#-- and r.ev_enabled <> %(enabled)s 
-#and r.rulename  in (
-	#select  
-	#'route_rule_bsms_in_p' 
-	#|| extract(year from now()+ interval '%(week)s week') 
-	#|| 'w' || extract(week from now() + interval '%(week)s week') 
-	#as  rulename
-	#)
-#order by r.rulename 
-#limit 1
-#;
-#""")
 
 
-
-if not vc_debug : print "["+str(datetime.datetime.now())+"] : Debug set to false at " + os.path.abspath(__file__) 
-
-if vc_sql_debug : print "["+str(datetime.datetime.now())+"] : SQL: ${0!s}".format(cur_aegir.mogrify(query_rule,{'week' : weeks_to_deactivate,'enabled' : "D",'tablename': table_name_base}))
-
-
-try:
-	# 2013-08-10.21.54.14 check rule to deactivate
-	cur_aegir.execute(query_rule,{'week' : weeks_to_deactivate,'enabled' : "D",'tablename': table_name_base})
-except psycopg2.Error, e:
-	if vc_debug :
-		print ("["+str(datetime.datetime.now())+"] : {0!s} conencting to {1!s} at {2!s} as user {3!s}".
-			format(
-				e.args[0].rstrip('\r\n'),
-				config_aegir.get('host').rstrip('\r\n'),
-				config_aegir.get('dbname').rstrip('\r\n'),
-				config_aegir.get('user').rstrip('\r\n'),
-			)
-		)
-
-rec_deactiavate = cur_aegir.fetchone()
-
-#if vc_debug : print "rules found : [{0!s}]".format(cur_aegir.rowcount)
-#if vc_debug : print "active : [{0!s}]".format(rec_deactiavate['active'])
-
-if int(cur_aegir.rowcount) > 0:
-	# tva e O 
-	if rec_deactiavate['active'] == 'O':
-		print "We have active rule {0!s} to deactivate".format(rec_deactiavate['rulename'])
-		sql_to_deactivete="alter table {0!s} disable rule {1!s};".format(table_name_base,rec_deactiavate['rulename'])
-		try:
-			cur_aegir.execute(sql_to_deactivete)
-			if vc_debug : print "["+str(datetime.datetime.now())+"] : executed '{0!s}'".format(sql_to_deactivete)
-		except psycopg2.Error, e:
-			#if vc_debug :
-			print ("["+str(datetime.datetime.now())+"] : ERROR : {0!s} on executing of {1!s}".
+for week in xrange(-weeks_to_deactivate,-1):
+	#print "["+str(datetime.datetime.now())+"] : {0!s}".format(week)
+	try:
+		# 2013-08-10.21.54.14 check rule to deactivate
+		if vc_debug : print "["+str(datetime.datetime.now())+"] : Checking is the rule for {0!s} week is ebabled..".format(week)
+		if vc_sql_debug : print "["+str(datetime.datetime.now())+"] : SQL: {0!s}".format(cur_aegir.mogrify(query_rule,{'week' : week,'enabled' : "D",'tablename': table_name_base}))
+		cur_aegir.execute(query_rule,{'week' : week,'enabled' : "D",'tablename': table_name_base})
+	except psycopg2.Error, e:
+		if vc_debug :
+			print ("["+str(datetime.datetime.now())+"] : {0!s} conencting to {1!s} at {2!s} as user {3!s}".
 				format(
 					e.args[0].rstrip('\r\n'),
-					sql_to_deactivete.rstrip('\r\n'),
+					config_aegir.get('host').rstrip('\r\n'),
+					config_aegir.get('dbname').rstrip('\r\n'),
+					config_aegir.get('user').rstrip('\r\n'),
 				)
 			)
-
-	else:
-		if vc_debug : print "["+str(datetime.datetime.now())+"] : We have inactive rule {0!s}".format(rec_deactiavate['rulename'])
-else:
-	if vc_debug : print "["+str(datetime.datetime.now())+"] : We dont have rule serving {0!s} weeks".format(weeks_to_deactivate)
+	rec_deactiavate = cur_aegir.fetchone()
 	
-
-#query_table_is_inherited = ("""
-
-#SELECT 
-#pg_inherits.*
-#, c.relname AS child
-#, p.relname AS parent
-#FROM
-    #pg_inherits JOIN pg_class AS c ON (inhrelid=c.oid)
-    #JOIN pg_class as p ON (inhparent=p.oid) 
-    #where true 
-    #and p.relname = %(table_name_base)s
-    #and c.relname = %(inherited_table)s
-#;
-#""")
-#query_table_exists = ("""
-#select 
-	#table_name 
-	#from information_schema.tables 
-	#where true 
-	#and table_name=%(inherited_table)s
-#;
-#""")
-
-#alter_table_inherit = ("""
-#alter table 
-	#%s 
-	#%s inherit
-	#%s
-#;
-#""")
-
-#create_trigger_on_table_inherited = ("""
-#CREATE TRIGGER counters_%s
-  #AFTER INSERT OR DELETE
-  #ON %s
-  #FOR EACH ROW
-  #EXECUTE PROCEDURE update_counters_bsmsin_trigger();
-#""")
-
-for week in xrange(1,5):
+	if int(cur_aegir.rowcount) > 0:
+		# tva e O 
+		if rec_deactiavate['active'] == 'O':
+			print "We have active rule {0!s} to deactivate".format(rec_deactiavate['rulename'])
+			sql_to_deactivete="alter table {0!s} disable rule {1!s};".format(table_name_base,rec_deactiavate['rulename'])
+			try:
+				cur_aegir.execute(sql_to_deactivete)
+				if vc_debug : print "["+str(datetime.datetime.now())+"] : executed '{0!s}'".format(sql_to_deactivete)
+			except psycopg2.Error, e:
+				#if vc_debug :
+				print ("["+str(datetime.datetime.now())+"] : ERROR : {0!s} on executing of {1!s}".
+					format(
+						e.args[0].rstrip('\r\n'),
+						sql_to_deactivete.rstrip('\r\n'),
+					)
+				)
+	
+		else:
+			if vc_debug : print "["+str(datetime.datetime.now())+"] : We have inactive rule {0!s}".format(rec_deactiavate['rulename'])
+	else:
+		if vc_debug : print "["+str(datetime.datetime.now())+"] : We dont have rule serving {0!s} weeks".format(week)
+	
+# Start checking inherited tables
+if vc_debug : print "["+str(datetime.datetime.now())+"] : Start checking inherited tables {0!s} weeks ahead..".format(weeks_to_activate-1)
+for week in xrange(1,weeks_to_activate):
 	inherited_table = (datetime.datetime.today() + datetime.timedelta(weeks=week)).strftime("bsms_in_p%Yw%U")
 	if vc_debug : print "["+str(datetime.datetime.now())+"] : Checking inherited table {0!s} existance..".format(inherited_table)
 	if vc_sql_debug : print "["+str(datetime.datetime.now())+"] : SQL: {0!s}".format(cur_aegir.mogrify(query_table_exists,{'inherited_table' : inherited_table}))
@@ -174,7 +111,7 @@ for week in xrange(1,5):
 		sys.exit(1)
 	if int(cur_aegir.rowcount) > 0:
 		if vc_debug : print "["+str(datetime.datetime.now())+"] : Table {0!s} exists.".format(inherited_table)
-		if vc_debug : print "["+str(datetime.datetime.now())+"] : Checking if table {0!s} inherits {1!s}".format(table_name_base,inherited_table)
+		if vc_debug : print "["+str(datetime.datetime.now())+"] : Checking if table {1!s} inherits {0!s}".format(table_name_base,inherited_table)
 		try:
 			cur_aegir.execute(query_table_is_inherited,{'inherited_table' : inherited_table,'table_name_base' : table_name_base})
 		except psycopg2.Error, e:
@@ -192,7 +129,7 @@ for week in xrange(1,5):
 			if vc_debug : print "["+str(datetime.datetime.now())+"] : Table {1!s} does not inherit {0!s}".format(table_name_base,inherited_table)
 			if vc_debug : print "["+str(datetime.datetime.now())+"] : Altering table {1!s} to inherit {0!s}".format(table_name_base,inherited_table)
 			alter_table_inherit_tmp = alter_table_inherit % (inherited_table,'',table_name_base);
-			if vc_debug : print cur_aegir.mogrify(alter_table_inherit_tmp)
+			if vc_sql_debug : print cur_aegir.mogrify(alter_table_inherit_tmp)
 			try:
 				cur_aegir.execute(alter_table_inherit_tmp)
 				if vc_sql_debug : print cur_aegir.mogrify(alter_table_inherit_tmp)
@@ -205,6 +142,50 @@ for week in xrange(1,5):
 						)
 					)
 				sys.exit(1)
+		# Check corresponsding rule 
+		try:
+			# 2013-08-10.21.54.14 check rule to activate
+			if vc_debug : print "["+str(datetime.datetime.now())+"] : Checking is the rule for {0!s} week is ebabled..".format(week)
+			if vc_sql_debug : print "["+str(datetime.datetime.now())+"] : SQL: {0!s}".format(cur_aegir.mogrify(query_rule,{'week' : week,'enabled' : "D",'tablename': table_name_base}))
+			cur_aegir.execute(query_rule,{'week' : week,'enabled' : "D",'tablename': table_name_base})
+		except psycopg2.Error, e:
+			if vc_debug :
+				print ("["+str(datetime.datetime.now())+"] : {0!s} conencting to {1!s} at {2!s} as user {3!s}".
+					format(
+						e.args[0].rstrip('\r\n'),
+						config_aegir.get('host').rstrip('\r\n'),
+						config_aegir.get('dbname').rstrip('\r\n'),
+						config_aegir.get('user').rstrip('\r\n'),
+					)
+				)
+		rec_actiavate = cur_aegir.fetchone()
+		
+		if int(cur_aegir.rowcount) > 0:
+			# tva e O 
+			if rec_actiavate['active'] == 'D':
+				if vc_debug : print "["+str(datetime.datetime.now())+"] : We have active rule {0!s} to activate".format(rec_actiavate['rulename'])
+				sql_to_activete="alter table {0!s} enable rule {1!s};".format(table_name_base,rec_actiavate['rulename'])
+				try:
+					cur_aegir.execute(sql_to_activete)
+					if vc_debug : print "["+str(datetime.datetime.now())+"] : executed '{0!s}'".format(sql_to_activete)
+				except psycopg2.Error, e:
+					#if vc_debug :
+					print ("["+str(datetime.datetime.now())+"] : ERROR : {0!s} on executing of {1!s}".
+						format(
+							e.args[0].rstrip('\r\n'),
+							sql_to_activete.rstrip('\r\n'),
+						)
+					)
+		
+			else:
+				if vc_debug : print "["+str(datetime.datetime.now())+"] : We have active rule {0!s}".format(rec_actiavate['rulename'])
+		else:
+			if vc_debug : print "["+str(datetime.datetime.now())+"] : We dont have rule serving {0!s} weeks".format(week)
+	else:
+		if vc_debug : print "["+str(datetime.datetime.now())+"] : Table {0!s} does NO exist.".format(inherited_table)
+		#if vc_debug : print "["+str(datetime.datetime.now())+"] : To be executed SQL: ".format(create_missing_inherited_table.format(inherited_table,))
+
+
 
 cur_aegir.close()
 conn_aegir.close()
--- vars.py
+++ vars.py
@@ -17,6 +17,7 @@ and r.rulename  in (
 	|| extract(year from now()+ interval '%(week)s week') 
 	|| 'w' || extract(week from now() + interval '%(week)s week') 
 	as  rulename
+	limit 1
 	)
 order by r.rulename 
 limit 1
@@ -63,3 +64,195 @@ CREATE TRIGGER counters_%s
   EXECUTE PROCEDURE update_counters_bsmsin_trigger();
 """)
 
+	
+create_missing_inherited_table= ("""
+-- DROP TABLE {0!s};
+
+CREATE TABLE {0!s}
+(
+  id bigint NOT NULL DEFAULT nextval('bsms_in_id_seq'::regclass),
+  client_sms_id character varying(100) NOT NULL,
+  ext_id character varying(100) NOT NULL,
+  msisdn bigint NOT NULL DEFAULT 0,
+  shortcode character varying(11) NOT NULL,
+  mesg text,
+  operator_id smallint NOT NULL DEFAULT 0,
+  otime timestamp without time zone NOT NULL,
+  ptime timestamp without time zone,
+  processed boolean DEFAULT false,
+  service_id bigint NOT NULL DEFAULT 0,
+  service_url text,
+  service_url_responce text,
+  request_ip cidr DEFAULT '127.0.0.1/32'::cidr,
+  forced_smsc character varying(200) DEFAULT 'infopipTEST'::character varying,
+  cyrilic smallint NOT NULL DEFAULT 0,
+  wap_push smallint NOT NULL DEFAULT 0,
+  polytype_responce xml,
+  priority smallint DEFAULT 2,
+  flash smallint DEFAULT 0,
+  polytype_id character varying(60),
+  retry bigint DEFAULT 0,
+  hlr integer DEFAULT 0,
+  validity integer DEFAULT 1440,
+  token character varying(100),
+  CONSTRAINT {0!s}_id_pkey PRIMARY KEY (id ),
+  CONSTRAINT {0!s}_uniq_ext_id_sid UNIQUE (ext_id , service_id ),
+--  CONSTRAINT {0!s}_partition_check CHECK (otime >= '2013-09-23 00:00:00'::timestamp without time zone AND otime < '2013-09-30 00:00:00'::timestamp without time zone)
+  CONSTRAINT {0!s}_partition_check CHECK (otime >= '{1!s} 00:00:00'::timestamp without time zone AND otime < '{2!s} 00:00:00'::timestamp without time zone)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE {0!s}
+  OWNER TO seik;
+GRANT ALL ON TABLE {0!s} TO seik;
+GRANT ALL ON TABLE {0!s} TO mtel;
+GRANT INSERT, TRIGGER ON TABLE {0!s} TO raiffeisen;
+
+-- Index: {0!s}.{0!s}_client_sms_id_idx
+
+-- DROP INDEX {0!s}.{0!s}_client_sms_id_idx;
+
+CREATE INDEX {0!s}_client_sms_id_idx
+  ON {0!s}
+  USING btree
+  (client_sms_id COLLATE pg_catalog."default" );
+
+-- Index: {0!s}.{0!s}_ext_id_idx
+
+-- DROP INDEX {0!s}.{0!s}_ext_id_idx;
+
+CREATE INDEX {0!s}_ext_id_idx
+  ON {0!s}
+  USING btree
+  (ext_id COLLATE pg_catalog."default" );
+
+-- Index: {0!s}.{0!s}_forced_smsc_idx
+
+-- DROP INDEX {0!s}.{0!s}_forced_smsc_idx;
+
+CREATE INDEX {0!s}_forced_smsc_idx
+  ON {0!s}
+  USING btree
+  (forced_smsc COLLATE pg_catalog."default" );
+
+-- Index: {0!s}.{0!s}_msisdn_idx
+
+-- DROP INDEX {0!s}.{0!s}_msisdn_idx;
+
+CREATE INDEX {0!s}_msisdn_idx
+  ON {0!s}
+  USING btree
+  (msisdn );
+
+-- Index: {0!s}.{0!s}_oday_idx
+
+-- DROP INDEX {0!s}.{0!s}_oday_idx;
+
+CREATE INDEX {0!s}_oday_idx
+  ON {0!s}
+  USING btree
+  (date_part('day'::text, otime) );
+
+-- Index: {0!s}.{0!s}_oeyar_idx
+
+-- DROP INDEX {0!s}.{0!s}_oeyar_idx;
+
+CREATE INDEX {0!s}_oeyar_idx
+  ON {0!s}
+  USING btree
+  (date_part('year'::text, otime) );
+
+-- Index: {0!s}.{0!s}_omonth_idx
+
+-- DROP INDEX {0!s}.{0!s}_omonth_idx;
+
+CREATE INDEX {0!s}_omonth_idx
+  ON {0!s}
+  USING btree
+  (date_part('month'::text, otime) );
+
+-- Index: {0!s}.{0!s}_otime_idx
+
+-- DROP INDEX {0!s}.{0!s}_otime_idx;
+
+CREATE INDEX {0!s}_otime_idx
+  ON {0!s}
+  USING btree
+  (otime );
+
+-- Index: {0!s}.{0!s}_polytype_id
+
+-- DROP INDEX {0!s}.{0!s}_polytype_id;
+
+CREATE INDEX {0!s}_polytype_id
+  ON {0!s}
+  USING btree
+  (polytype_id COLLATE pg_catalog."default" );
+
+-- Index: {0!s}.{0!s}_priority_idx
+
+-- DROP INDEX {0!s}.{0!s}_priority_idx;
+
+CREATE INDEX {0!s}_priority_idx
+  ON {0!s}
+  USING btree
+  (priority );
+
+-- Index: {0!s}.{0!s}_processed_idx
+
+-- DROP INDEX {0!s}.{0!s}_processed_idx;
+
+CREATE INDEX {0!s}_processed_idx
+  ON {0!s}
+  USING btree
+  (processed );
+
+-- Index: {0!s}.{0!s}_service_id_idx
+
+-- DROP INDEX {0!s}.{0!s}_service_id_idx;
+
+CREATE INDEX {0!s}_service_id_idx
+  ON {0!s}
+  USING btree
+  (service_id );
+
+-- Index: {0!s}.{0!s}_shortcode_idx
+
+-- DROP INDEX {0!s}.{0!s}_shortcode_idx;
+
+CREATE INDEX {0!s}_shortcode_idx
+  ON {0!s}
+  USING btree
+  (shortcode COLLATE pg_catalog."default" );
+
+-- Index: {0!s}.new_{0!s}_otime_idx
+
+-- DROP INDEX {0!s}.new_{0!s}_otime_idx;
+
+CREATE INDEX new_{0!s}_otime_idx
+  ON {0!s}
+  USING btree
+  (date_trunc('month'::text, otime) );
+
+-- Index: {0!s}.test_idx_protelecom_{0!s}
+
+-- DROP INDEX {0!s}.test_idx_protelecom_{0!s};
+
+CREATE INDEX test_idx_protelecom_{0!s}
+  ON {0!s}
+  USING btree
+  (service_id , date_part('year'::text, otime) , date_part('month'::text, otime) );
+
+
+-- Trigger: counters_{0!s} on {0!s}
+
+-- DROP TRIGGER counters_{0!s} ON {0!s};
+
+CREATE TRIGGER counters_{0!s}
+  AFTER INSERT OR DELETE
+  ON {0!s}
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_counters_bsmsin_trigger();
+
+""")
