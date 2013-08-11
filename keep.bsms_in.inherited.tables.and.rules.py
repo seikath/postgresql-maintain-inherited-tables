e7b79c658233ff0edfddc175358b2da2ada562ca
seikath <seikath@gmail.com>
2013-08-11 18:51:28 +0300

    added inherit next 5 tables

--- keep.bsms_in.inherited.tables.and.rules.py
+++ keep.bsms_in.inherited.tables.and.rules.py
@@ -1,48 +1,12 @@
 # ecoding=utf8
 
-# 2013-07-27.00.46.56
-############################################################################################
-# cd /opt/proceed.bsms/psycopg2-2.5.1-1
-# git remote add origin ssh://git@bitbucket.org/seikath/aegir.bsms.psycopg2-2.5.1-1.git
-# git push -u origin --all   # to push up the repo for the first time
-############################################################################################
-# OR start from scratch
-# mkdir /path/to/your/project
-# cd /path/to/your/project
-# git init
-# git remote add origin ssh://git@bitbucket.org/seikath/aegir.bsms.psycopg2-2.5.1-1.git
-# git add *
-# git commit -m "psycopg2-2.5.1-1.git"
-############################################################################################
-# root@aegir.voicecom.bg:[Sat Jul 27 02:02:04]:[/opt/proceed.bsms/psycopg2-2.5.1-1]$ git push -u origin --all
-# Counting objects: 3, done.
-# Delta compression using up to 2 threads.
-# Compressing objects: 100% (2/2), done.
-# Writing objects: 100% (3/3), 439 bytes, done.
-# Total 3 (delta 0), reused 0 (delta 0)
-# To ssh://git@bitbucket.org/seikath/aegir.bsms.psycopg2-2.5.1-1.git
-# * [new branch]      master -> master
-# Branch master set up to track remote branch master from origin.
-############################################################################################
-## git checkout -b lean_db_conection
-## git push origin lean_db_conection
-#  https://drupal.org/node/1066342
-# http://initd.org/psycopg/docs/module.html#exceptions
-# http://initd.org/psycopg/docs/usage.html
-
-# The basic connection parameters are:
-
-# dbname – the database name (only in the dsn string)
-# database – the database name (only as keyword argument)
-# user – user name used to authenticate
-# password – password used to authenticate
-# host – database host address (defaults to UNIX socket if not provided)
-# port – connection port number (defaults to 5432 if not provided)
-
 import psycopg2
 import psycopg2.extras
 import sys
-from datetime import datetime
+#import datetime
+from datetime import timedelta,date
+import datetime
+#import date
 import ConfigParser
 import os
 os.environ['PGCONNECT_TIMEOUT'] = '5'
@@ -51,8 +15,6 @@ os.environ['PGCONNECT_TIMEOUT'] = '5'
 ### CONFIG START 
 config = ConfigParser.SafeConfigParser()
 # get the config at the db.config file in the same directory
-#print os.path.basename(os.path.dirname(os.path.abspath(__file__)))
-# config.read(os.path.dirname(os.path.abspath(__file__))+'.c')
 config_file=os.path.dirname(os.path.abspath(__file__))+'/'+os.path.basename(os.path.dirname(os.path.abspath(__file__)))+'.cfg'
 
 #print config_file
@@ -64,7 +26,8 @@ try:
 	weeks_to_activate = config.getint('rule-conf','weeks_to_activate')
 	table_name_base = config.get('rule-conf','table_name_base')
 	vc_debug = config.getboolean('rule-conf','vc_debug')
-	if vc_debug : print "["+str(datetime.now())+"] : db config : {0!s}".format(config_aegir)
+	vc_sql_debug = config.getboolean('rule-conf','vc_sql_debug')
+	if vc_sql_debug : print "["+str(datetime.datetime.now())+"] : db config : {0!s}".format(config_aegir)
 except ConfigParser.Error, e:
 	print "["+str(datetime.now())+"] : " + "Error : %s" % (e)
 	sys.exit(1)
@@ -74,7 +37,7 @@ try:
     conn_aegir = psycopg2.connect(**config_aegir)
     conn_aegir.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 except:
-    print "["+str(datetime.now())+"] : I am unable to connect:: %s !!".format(config_aegir)
+    print "["+str(datetime.now())+"] : Not able to connect:: %s !!".format(config_aegir)
     sys.exit(1)
 
 # sys.exit (0)
@@ -104,9 +67,10 @@ limit 1
 """)
 
 
-if not vc_debug : print "["+str(datetime.now())+"] : Debug set to false at " + os.path.abspath(__file__) 
 
-if vc_debug : print "["+str(datetime.now())+"] : SQL: ${0!s}".format(cur_aegir.mogrify(query_rule,{'week' : weeks_to_deactivate,'enabled' : "D",'tablename': table_name_base}))
+if not vc_debug : print "["+str(datetime.datetime.now())+"] : Debug set to false at " + os.path.abspath(__file__) 
+
+if vc_sql_debug : print "["+str(datetime.datetime.now())+"] : SQL: ${0!s}".format(cur_aegir.mogrify(query_rule,{'week' : weeks_to_deactivate,'enabled' : "D",'tablename': table_name_base}))
 
 
 try:
@@ -114,7 +78,7 @@ try:
 	cur_aegir.execute(query_rule,{'week' : weeks_to_deactivate,'enabled' : "D",'tablename': table_name_base})
 except psycopg2.Error, e:
 	if vc_debug :
-		print ("["+str(datetime.now())+"] : {0!s} conencting to {1!s} at {2!s} as user {3!s}".
+		print ("["+str(datetime.datetime.now())+"] : {0!s} conencting to {1!s} at {2!s} as user {3!s}".
 			format(
 				e.args[0].rstrip('\r\n'),
 				config_aegir.get('host').rstrip('\r\n'),
@@ -135,10 +99,10 @@ if int(cur_aegir.rowcount) > 0:
 		sql_to_deactivete="alter table {0!s} disable rule {1!s};".format(table_name_base,rec_deactiavate['rulename'])
 		try:
 			cur_aegir.execute(sql_to_deactivete)
-			if vc_debug : print "["+str(datetime.now())+"] : executed '{0!s}'".format(sql_to_deactivete)
+			if vc_debug : print "["+str(datetime.datetime.now())+"] : executed '{0!s}'".format(sql_to_deactivete)
 		except psycopg2.Error, e:
 			#if vc_debug :
-			print ("["+str(datetime.now())+"] : ERROR : {0!s} on executing of {1!s}".
+			print ("["+str(datetime.datetime.now())+"] : ERROR : {0!s} on executing of {1!s}".
 				format(
 					e.args[0].rstrip('\r\n'),
 					sql_to_deactivete.rstrip('\r\n'),
@@ -146,46 +110,93 @@ if int(cur_aegir.rowcount) > 0:
 			)
 
 	else:
-		if vc_debug : print "["+str(datetime.now())+"] :We have inactive rule {0!s}".format(rec_deactiavate['rulename'])
+		if vc_debug : print "["+str(datetime.datetime.now())+"] : We have inactive rule {0!s}".format(rec_deactiavate['rulename'])
 else:
-	if vc_debug : print "["+str(datetime.now())+"] : We dont have rule serving {0!s} weeks".format(weeks_to_deactivate)
+	if vc_debug : print "["+str(datetime.datetime.now())+"] : We dont have rule serving {0!s} weeks".format(weeks_to_deactivate)
 	
-#print rec['rulename']
 
-#print rec['active']
+query_table_is_inherited = ("""
 
+SELECT 
+pg_inherits.*
+, c.relname AS child
+, p.relname AS parent
+FROM
+    pg_inherits JOIN pg_class AS c ON (inhrelid=c.oid)
+    JOIN pg_class as p ON (inhparent=p.oid) 
+    where true 
+    and p.relname = %(table_name_base)s
+    and c.relname = %(inherited_table)s
+;
+""")
+query_table_exists = ("""
+select 
+	table_name 
+	from information_schema.tables 
+	where true 
+	and table_name=%(inherited_table)s
+;
+""")
 
-
-
+alter_table_inherit = ("""
+alter table 
+	%s 
+	%s inherit
+	%s
+;
+""")
+vc_sql_debug = False
+for week in xrange(1,5):
+	inherited_table = (datetime.datetime.today() + datetime.timedelta(weeks=week)).strftime("bsms_in_p%Yw%U")
+	if vc_debug : print "["+str(datetime.datetime.now())+"] : Checking inherited table {0!s} existance..".format(inherited_table)
+	if vc_sql_debug : print "["+str(datetime.datetime.now())+"] : SQL: {0!s}".format(cur_aegir.mogrify(query_table_exists,{'inherited_table' : inherited_table}))
+	try:
+		cur_aegir.execute(query_table_exists,{'inherited_table' : inherited_table})
+	except psycopg2.Error, e:
+		if vc_debug :
+			print ("["+str(datetime.datetime.now())+"] : ERROR {0!s} executing {1!s}".
+				format(
+					e.args[0].rstrip('\r\n'),
+					query_table_exists
+				)
+			)
+		sys.exit(1)
+	if int(cur_aegir.rowcount) > 0:
+		if vc_debug : print "["+str(datetime.datetime.now())+"] : Table {0!s} exists.".format(inherited_table)
+		if vc_debug : print "["+str(datetime.datetime.now())+"] : Checking if table {0!s} inherits {1!s}".format(table_name_base,inherited_table)
+		try:
+			cur_aegir.execute(query_table_is_inherited,{'inherited_table' : inherited_table,'table_name_base' : table_name_base})
+		except psycopg2.Error, e:
+			if vc_debug :
+				print ("["+str(datetime.datetime.now())+"] : ERROR {0!s} executing {1!s}".
+					format(
+						e.args[0].rstrip('\r\n'),
+						query_table_is_inherited
+					)
+				)
+			sys.exit(1)
+		if int(cur_aegir.rowcount) > 0:
+			if vc_debug : print "["+str(datetime.datetime.now())+"] : Table {1!s} inherits {0!s}".format(table_name_base,inherited_table)
+		else:
+			if vc_debug : print "["+str(datetime.datetime.now())+"] : Table {1!s} does not inherit {0!s}".format(table_name_base,inherited_table)
+			if vc_debug : print "["+str(datetime.datetime.now())+"] : Altering table {1!s} to inherit {0!s}".format(table_name_base,inherited_table)
+			alter_table_inherit_tmp = alter_table_inherit % (inherited_table,'',table_name_base);
+			if vc_debug : print cur_aegir.mogrify(alter_table_inherit_tmp)
+			try:
+				cur_aegir.execute(alter_table_inherit_tmp)
+				if vc_sql_debug : print cur_aegir.mogrify(alter_table_inherit_tmp)
+			except psycopg2.Error, e:
+				if vc_debug :
+					print ("["+str(datetime.datetime.now())+"] : ERROR {0!s} executing {1!s}".
+						format(
+							e.args[0].rstrip('\r\n'),
+							query_table_is_inherited
+						)
+					)
+				sys.exit(1)
 
 cur_aegir.close()
 conn_aegir.close()
 sys.exit (0)
 
 
-
-# CONFIGs, SQL etc ========= [ end ] 
-
-# initialize db conection to nova
-#try:
-	#cnx_aegir = MySQLdb.connect(**config_nova)
-	## open nova cursor
-	#cursor_nova = cnx_nova.cursor()
-#except MySQLdb.Error, e:
-	#print "["+str(datetime.now())+"] : " + "Error %d: %s" % (e.args[0], e.args[1])
-	#sys.exit (1)
-	
-# start the data processing
-#try:
-	#cursor_nova.execute(query)
-	## initialize db conection to pdns
-	#try:
-		#cnx_pdns = MySQLdb.connect(**config_pdns)
-		## open pdns cursor
-		#cursor_pdns = cnx_pdns.cursor()
-	#except MySQLdb.Error, e:
-		#print "["+str(datetime.now())+"] : " + "Error %d: %s" % (e.args[0], e.args[1])
-		#sys.exit (1)
-#except Exception, e:
-	#print "["+str(datetime.now())+"] : " + repr(e)
-	#sys.exit (1)
